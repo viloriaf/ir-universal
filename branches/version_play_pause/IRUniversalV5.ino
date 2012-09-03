22542a24906f2575797dbb3ca00b7a1cc3ae87f7
agarciaf7@gmail.com <agarciaf7@gmail.com@ed7a456e-23b7-3d0f-6bbd-2824109e7108>
2012-09-03 17:21:23 +0000

    Pongo buzzer y variable para controlar que haga play y pause

--- branches/version_play_pause/IRUniversalV5.ino
+++ branches/version_play_pause/IRUniversalV5.ino
@@ -28,6 +28,21 @@
 #include <EEPROM.h>
 #include <EEPROMAnything.h>// http://arduino.cc/playground/Code/EEPROMWriteAnything
 
+//**************Tonos del buzzer
+#include "pitches.h" //Para tonos del buzzer
+ // notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4,4,4,4,4 };
+  
+int BUZZER_PIN = 9;
+//************* Fin tonos
+  
+boolean pause = true; //Sirve para alternar entre play o pause cada vez que el sensor de distancia detecta algo
+
 int RECV_PIN = 12; //IR detector/demodulator
 int STATUS_PIN = 13; //Visible LED
 
@@ -77,8 +92,15 @@ void sensorProcess(){
         //Second reading to confirm object proximity
         Serial.print("Second reading:");
         Serial.println(sensorValue); 
-        sendCode(0); //Sending first stored code
-        delay(2000); //delay between readings to avoid errors
+        if (pause){
+          sendCode(0); //Sending first stored code
+          pause=false;
+        }else{
+          sendCode(1); //Sending second stored code
+          pause=true;
+        }
+        musiquita();
+        //delay(2000); //delay between readings to avoid errors
     }
   }else{
     Serial.println();
@@ -340,6 +362,27 @@ void buttonsProcess(){
 void codesReceiveProcess(){
   if (digitalRead(inputs[0]) == HIGH && digitalRead(inputs[1]) == HIGH){  
     receiveCodes();
+  }
+}
+
+// Emite la musica por el buzzer
+void musiquita(){
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(BUZZER_PIN, melody[thisNote],noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(BUZZER_PIN);
+    digitalWrite(BUZZER_PIN, LOW);
   }
 }
 
